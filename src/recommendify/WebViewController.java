1e07551c194daf672da44046a01861e6d8e56eeb
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-20 05:37:49 -0800

    set up infrastructure for displaying succesful login alert

--- src/recommendify/WebViewController.java
+++ src/recommendify/WebViewController.java
@@ -2,9 +2,16 @@ package recommendify;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WebViewController {
     private Spotify spotify = new Spotify();
@@ -27,6 +34,21 @@ public class WebViewController {
         System.out.println("Got access token: " + code);
         System.out.println("Closing login window...");
         ((Button)e.getTarget()).getScene().getWindow().hide();
+        try {
+            renderLoginSuccess(e);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void renderLoginSuccess(ActionEvent event) throws IOException {
+        Parent loginSuccessParent = FXMLLoader.load(getClass().getResource("auth_success.fxml"));
+        Scene loginSuccessScene = new Scene(loginSuccessParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(loginSuccessScene);
+        window.show();
     }
 
     private static String getQueryCode(String query) {
