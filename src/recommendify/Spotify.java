0a6e33ad6a63a4e4ea037a12eb3247f511730b4d
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 17:42:50 -0800

    starting architecture for creating properties file in order to store api keys

--- src/recommendify/Spotify.java
+++ src/recommendify/Spotify.java
@@ -1,4 +1,11 @@
 package recommendify;
 
 public class Spotify {
+    private static final String clientId;
+    private static final String clientSecret;
+
+    public Spotify(String clientId, String clientSecret) {
+        this.clientId = loadVal;
+        this.clientSecret = loadVal;
+    }
 }
