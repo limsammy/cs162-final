ae3054156f77a3c617b03222c3d68aed29836962
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 18:27:59 -0800

    wrote pressLoadConfigBtn logic

--- src/recommendify/LoginController.java
+++ src/recommendify/LoginController.java
@@ -4,11 +4,24 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class LoginController {
+    Spotify spotify = new Spotify();
+
     public void setClientIdLabel(ActionEvent e) throws IOException {
 
     }
 
     public void setClientSecretLabel(ActionEvent e) throws IOException {
 
+    }
+
+    public void pressLoadConfigBtn(ActionEvent e) throws IOException {
+        System.out.println("Loading config...");
+        try {
+            spotify.loadProperties();
+            System.out.println("config.properties file loaded succesfully");
+        } catch (IOException ex) {
+            System.out.println("The config.properties file does not exist, default properties loaded." +
+                    "Please set these values manually");
+        }
     }
 }
