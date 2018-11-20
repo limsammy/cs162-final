e428b9ecc5f69a1c2137034588d256f0c3c5c967
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 18:18:51 -0800

    added fx id's to components

--- src/recommendify/LoginController.java
+++ src/recommendify/LoginController.java
@@ -1,4 +1,14 @@
 package recommendify;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class LoginController {
+    public void setClientIdLabel(ActionEvent e) throws IOException {
+
+    }
+
+    public void setClientSecretLabel(ActionEvent e) throws IOException {
+
+    }
 }
