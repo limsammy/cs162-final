43df5e1eff1c0ad7423d66655f1a7f1e55bfdb0e
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 21:01:49 -0800

    succesfully generating auth URI

--- src/recommendify/Spotify.java
+++ src/recommendify/Spotify.java
@@ -1,25 +1,39 @@
 package recommendify;
 
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.SpotifyHttpManager;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+
 import java.io.*;
+import java.net.URI;
 import java.util.Properties;
 
 public class Spotify {
     private static File configFile = new File("config.properties");
     private static Properties configProps;
+
     private static final String clientId = new String();
     private static final String clientSecret = new String();
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("https://example.com/spotify-redirect");
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .setRedirectUri(redirectUri)
+            .build();
+
+    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+            .show_dialog(true)
+            .build();
+
+    public static void authorizationCodeUri_Sync() {
+        final URI uri = authorizationCodeUriRequest.execute();
+
+        System.out.println("URI: " + uri.toString());
+    }
 
     public Spotify() {
-        // load config file into memory
-//        try {
-//            loadProperties();
-//        } catch (IOException ex) {
-//            System.out.println("The config.properties file does not exist, default properties loaded.");
-//        }
-//
-//        // set instance variables
-//        this.clientId = configProps.getProperty("clientId");
-//        this.clientSecret = configProps.getProperty("clientSecret");
+        authorizationCodeUri_Sync();
     }
 
     public Properties loadProperties() throws IOException {
