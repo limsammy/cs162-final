095e872be8b41ed400953a7d08cf7af64365f152
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 18:01:22 -0800

    wrote loadProperties method to load config file. I call this in the Spotify constructor to set clientId and clientSecret

--- src/recommendify/Spotify.java
+++ src/recommendify/Spotify.java
@@ -1,11 +1,41 @@
 package recommendify;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Spotify {
+    private static File configFile = new File("config.properties");
+    private static Properties configProps;
+
     private static final String clientId;
     private static final String clientSecret;
 
-    public Spotify(String clientId, String clientSecret) {
-        this.clientId = loadVal;
-        this.clientSecret = loadVal;
+    public Spotify() {
+        try {
+            loadProperties();
+        } catch (IOException ex) {
+            System.out.println("The config.properties file does not exist, default properties loaded.");
+        }
+
+        this.clientId = configProps.getProperty("clientId");
+        this.clientSecret = configProps.getProperty("clientSecret");
+    }
+
+    private static void loadProperties() throws IOException {
+        Properties defaultProps = new Properties();
+
+        // set default properties
+        defaultProps.setProperty("clientId", "PLEASE_SET_THIS");
+        defaultProps.setProperty("clientSecret", "PLEASE_SET_THIS");
+
+        configProps = new Properties(defaultProps);
+
+        // load properties from config file
+        InputStream inputStream = new FileInputStream(configFile);
+        configProps.load(inputStream);
+        inputStream.close();
     }
 }
