7aedd72c6a9509df038b5b90f7e872ec6ddef024
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-19 22:50:22 -0800

    minor changes

--- src/recommendify/Spotify.java
+++ src/recommendify/Spotify.java
@@ -12,9 +12,7 @@ public class Spotify {
     private static ConfigHelper configHelper = new ConfigHelper();
     private static Properties configProps;
 
-    private static String clientId = new String();
-    private static String clientSecret = new String();
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("https://example.com/spotify-redirect");
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://www.movemypaper.com/images/sucess.png");
 
     private static SpotifyApi spotifyApi;
 
@@ -39,11 +37,6 @@ public class Spotify {
         authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
                 .show_dialog(true)
                 .build();
-    }
-
-    public void setApiConfig(String clientId, String clientSecret) {
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
     }
 
     public static String getAuthUri() {
