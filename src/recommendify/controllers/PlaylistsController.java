e11b1807d5832b4aa6e364faeb37789ac27187b1
limsammy1@gmail.com <limsammy1@gmail.com>
2018-11-30 06:10:17 -0800

    Began writing base code for implementing table data of playlists

--- src/recommendify/controllers/PlaylistsController.java
+++ src/recommendify/controllers/PlaylistsController.java
@@ -1,19 +1,58 @@
 package recommendify.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PlaylistsController {
     private HashMap data;
 
     @FXML
-    private void initialize() {
+    private TableView<Map.Entry<String,String>> playlistTable;
+    @FXML
+    private TableColumn<Map.Entry<String, String>, String> playlistCol;
+    @FXML
+    private TableColumn trackCtCol;
+    @FXML
+    private TableColumn actionsCol;
 
+    @FXML
+    private void initialize() {
         populateTable(data);
     }
 
     private void populateTable(HashMap data) {
+        playlistCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Map.Entry<String, String>, String>, ObservableValue<String>>() {
 
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Map.Entry<String, String>, String> p) {
+                // this callback returns property for just one cell, you can't use a loop here
+                // for first column we use key
+                return new SimpleStringProperty(p.getValue().getKey());
+            }
+        });
+
+        TableColumn<Map.Entry<String, String>, String> column2 = new TableColumn<>("Value");
+        column2.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Map.Entry<String, String>, String>, ObservableValue<String>>() {
+
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Map.Entry<String, String>, String> p) {
+                // for second column we use value
+                return new SimpleStringProperty(p.getValue().getValue());
+            }
+        });
+
+        ObservableList<Map.Entry<String, String>> items = FXCollections.observableArrayList(data.entrySet());
+
+        playlistTable.getColumns().setAll(playlistCol, column2);
     }
+
 }
